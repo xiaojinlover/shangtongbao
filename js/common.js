4078fcb43652700792856ef258898f4d79ca77b7
jiangxujin <432853096@qq.com>
2017-08-11 17:36:36 +0800

    Signed-off-by: jiangxujin <432853096@qq.com>

--- js/common.js
+++ js/common.js
@@ -28,30 +28,33 @@ function removeHtmlTab(tab) {
  	tab = tab.replace(/</g,'');//删除所有HTML标签 
  	return tab.replace(/>/g,'');
 } 
+
 mui.plusReady(function() {
 	//系统状态栏
 	plus.navigator.setStatusBarBackground("#259cf0");
 	plus.navigator.setStatusBarStyle("UIStatusBarStyleBlackOpaque");
+
+
+
+	//plus.storage.setItem("UserId", 'admin');
+	//plus.storage.setItem("Password", '96E79218965EB72C92A549DD5A330112'); 
+	plus.storage.setItem("Ran", '123');
+	plus.storage.setItem("Sign", '4297F44B13955235245B2497399D7A93');
+	
+	if(plus.storage.getItem('UserId')==null&&plus.webview.currentWebview().id!='login'){ 
+		mui.openWindow({
+			url: 'login.html',
+			id: 'login',
+			createNew: true,
+			show: {
+				autoShow: true,
+				aniShow: false,
+				duration: 200
+			},
+			waiting: {
+				autoShow: false
+			}
+		}) 
+		return false; 
+	}
 })
-
-
-//plus.storage.setItem("UserId", 'admin');
-//plus.storage.setItem("Password", '96E79218965EB72C92A549DD5A330112');
-plus.storage.setItem("Ran", '123');
-plus.storage.setItem("Sign", '4297F44B13955235245B2497399D7A93');
-
-if(plus.storage.getItem('UserId')==null&&plus.webview.currentWebview().id!='login'){
-	mui.openWindow({
-		url: 'login.html',
-		id: 'login',
-		createNew: false,
-		show: {
-			autoShow: false,
-			aniShow: false,
-			duration: 200
-		},
-		waiting: {
-			autoShow: false
-		}
-	}) 
-}
